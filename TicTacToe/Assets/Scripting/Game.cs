cc8d76e290be748ca88477d7ec930740451fda31
DarkBird <josepokemon234@gmail.com>
2018-12-20 16:42:42 +0100

    Some initial set up

---
+++ TicTacToe/Assets/Scripting/Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game {
+
+	//Sets the board
+	public static byte[,] board = {
+		{ 0, 0, 0 },
+		{ 0, 0, 0 },
+		{ 0, 0, 0 }
+	};
+
+	public static bool turn = false; //false = Player1; true = Player 2
+
+	public static void TileClick (int x, int y) {
+		Debug.Log("Tile (" + x + "," + y + ") was clicked");
+	}
+
+}
---
+++ TicTacToe/Assets/Scripting/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+	private int x, y;
+
+	private void Start () {
+		string[] data = gameObject.name.Split(',');
+		x = int.Parse(data[0]);
+		y = int.Parse(data[1]);
+	}
+
+	private void OnMouseUp () {
+		Game.TileClick(x, y);
+	}
+}
