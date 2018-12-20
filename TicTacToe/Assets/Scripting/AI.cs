b4fb50d28541e15e20b76327ba321851e658c112
DarkBird <josepokemon234@gmail.com>
2018-12-20 19:20:46 +0100

    Fixed the stack overflow bug but it works just as bad as the previous one xD

--- TicTacToe/Assets/Scripting/AI.cs
+++ TicTacToe/Assets/Scripting/AI.cs
@@ -25,79 +25,15 @@ public static class AI {
 		Game.TileClick(owo.x, owo.y, tile);
 	}
 
-	/* private static Move minimax (int[,] board, int player) {
-
-		int winner = Game.CheckWinner(board);
-		if (winner == 1) {
-			Move ret = new Move();
-			ret.score = -10;
-			return ret;
-		}
-		else if (winner == 2) {
-			Move ret = new Move();
-			ret.score = 10;
-			return ret;
-		}
-
-		List<Vector2> freeTiles = GetFreeTiles(board);
-
-		List<Move> moves = new List<Move>();
-		for (int i = 0; i < freeTiles.Count; i++) {
-			//Creates a move at the free tile's position
-			Move move = new Move();
-			move.position = freeTiles[i];
-
-			//Sets the tile to the moving player
-			board[(int)move.position.x, (int)move.position.y] = player;
-
-			//Recursively iterates the minimax function
-			if (player == 1) {
-				Move result = minimax(board, 2);
-				move.score = result.score;
-			}
-			else {
-				Move result = minimax(board, 1);
-				move.score = result.score;
-			}
-
-			//Resets the tile
-			board[(int)move.position.x, (int)move.position.y] = 0;
-			moves.Add(move);
-		}
-
-
-		Move bestMove = new Move();
-		if (player == 2) {
-			var bestScore = -10000;
-			for (int i = 0; i < moves.Count; i++) {
-				if (moves[i].score > bestScore) {
-					bestScore = moves[i].score;
-					bestMove = moves[i];
-				}
-			}
-		}
-		else if (player == 1) {
-			var bestScore = 10000;
-			for (int i = 0; i < moves.Count; i++) {
-				if (moves[i].score < bestScore) {
-					bestScore = moves[i].score;
-					bestMove = moves[i];
-				}
-			}
-		}
-
-		return bestMove;
-	}*/
-
 	private static Move GetBestMove (int[,] board, bool turn) {
-		Move bestMove = new Move(1, 1, 0);
+		Move bestMove = new Move(1, 1, turn ? 10000 : -10000);
 
 		int winner = Game.CheckWinner(board);
 		if (winner == 2) {
-			return new Move(1, 1, -10);
+			return new Move(-1, -1, -10);
 		}
 		else if (winner == 1) {
-			return new Move(1, 1, 10);
+			return new Move(-1, -1, 10);
 		}
 
 		List<Vector2> freeTiles = GetFreeTiles(board);
@@ -107,6 +43,8 @@ public static class AI {
 			board[(int)tile.x, (int)tile.y] = turn ? 2 : 1;
 			
 			Move next = GetBestMove(board, !turn);
+			next.x = (int)tile.x;
+			next.y = (int)tile.y;
 			moves.Add(next);
 			
 			board[(int)tile.x, (int)tile.y] = 0;
@@ -120,13 +58,11 @@ public static class AI {
 			}
 		}
 		else {
-			if (turn) {
 			foreach (Move m in moves) {
 				if (m.score > bestMove.score) {
 					bestMove = m;
 				}
 			}
-		}
 		}
 
 		return bestMove;
